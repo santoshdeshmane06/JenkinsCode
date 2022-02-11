ec963684ae1f29b736d46352a5b80f90be40a3c6
Developer <test.example.com>
2022-02-11 13:39:47 +0000

    first commit

---
+++ number.sh
@@ -0,0 +1,6 @@
+echo "First number is $num1" 
+echo "Second number is $num2" 
+echo "Addition of both the numbers is : $((num1 + num2))"
+echo "This script will now be pulled from webhook" 
+echo " New changes added" 
+echo "absahch" 
